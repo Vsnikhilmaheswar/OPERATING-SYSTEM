d10327987c0b9d4497525a7eb3f5655e07fd9a8c
Vsnikhilmaheswar <84986865+Vsnikhilmaheswar@users.noreply.github.com>
2022-10-19 21:59:02 +0530

    Update priority.c

--- priority.c
+++ priority.c
@@ -1,70 +1,61 @@
 #include<stdio.h>
- 
+struct sjf
+{
+    int pid;
+    int btime;
+    int wtime;
+    int ttime;
+    int priority;
+}
+p[10];
 int main()
 {
-    int bt[20],p[20],wt[20],tat[20],pr[20],i,j,n,total=0,pos,temp,avg_wt,avg_tat;
-    printf("Enter Total Number of Process:");
+    int i,n,j;
+    struct sjf tmp;
+    int totwtime=0,totttime=0;
+    printf("enter the number of process");
     scanf("%d",&n);
-    printf("\nEnter Burst Time and Priority\n");
     for(i=0;i<n;i++)
     {
-        printf("\nP[%d]\n",i+1);
-        printf("Burst Time:");
-        scanf("%d",&bt[i]);
-        printf("Priority:");
-        scanf("%d",&pr[i]);
-        p[i]=i+1;           
+        printf("enter process id: P");
+        scanf("%d",&p[i].pid);
+        printf("enter burst time");
+        scanf("%d",&p[i].btime);
+        printf("enter priority");
+        scanf("%d",&p[i].priority);
     }
-    for(i=0;i<n;i++)
+    p[0].wtime=0;
+    p[0].ttime=p[0].btime;
+    for ( i = 0; i <n-1; i++)
     {
-        pos=i;
         for(j=i+1;j<n;j++)
         {
-            if(pr[j]<pr[pos])
-                pos=j;
+            if(p[i].priority>p[j].priority)
+            {
+             tmp=p[i];
+             p[i]=p[j];
+             p[j]=tmp;
+            }
         }
- 
-        temp=pr[i];
-        pr[i]=pr[pos];
-        pr[pos]=temp;
- 
-        temp=bt[i];
-        bt[i]=bt[pos];
-        bt[pos]=temp;
- 
-        temp=p[i];
-        p[i]=p[pos];
-        p[pos]=temp;
     }
- 
-    wt[0]=0; //waiting time for first process is zero
- 
-    //calculate waiting time
-    for(i=1;i<n;i++)
+    for ( i = 0; i < n; i++)
     {
-        wt[i]=0;
-        for(j=0;j<i;j++)
-            wt[i]+=bt[j];
- 
-        total+=wt[i];
+        p[i].wtime=p[i-1].wtime+p[i-1].btime;
+        p[i].ttime=p[i].wtime+p[i].btime;
+        totttime+=p[i].ttime;
+        totwtime+=p[i].wtime;
     }
- 
-    avg_wt=total/n;     
-   int total2=0;
- 
-    printf("\nProcess\t    Burst Time    \tWaiting Time\tTurnaround Time");
+
+    printf("pid     burst   wt    tat\n");
+for ( i = 0; i < n; i++)
+{
+printf("%d    %d     %d     %d\n",p[i].pid,p[i].btime,p[i].wtime,p[i].ttime);
+}
+printf("\n");
+   printf("0 ");
     for(i=0;i<n;i++)
     {
-        tat[i]=bt[i]+wt[i];     
-        total2+=tat[i];
-        printf("\nP[%d]\t\t  %d\t\t    %d\t\t\t%d",p[i],bt[i],wt[i],tat[i]);
+        printf(" p%d | %d ",p[i].pid,p[i].ttime);
     }
- 
-    avg_tat=total/n;     
-        printf("\n\ntotal Waiting Time=%d",total);
-            printf("\n\ntotal turn around Time=%d",total2);
-    printf("\n\nAverage Waiting Time=%d",avg_wt);
-    printf("\nAverage Turnaround Time=%d\n",avg_tat);
- 
-return 0;
+     
 }
